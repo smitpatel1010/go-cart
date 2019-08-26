4d84a135cf482b27e2508e3ddd3575b62da8d3ea
prateek <prateekcelly@gmail.com>
2019-08-26 16:19:52 +0530

    Added carddeck

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 import NavBar from "./navbar";
+import MainCategory from "./maincategory";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: ["Men", "Women", "Kids"]
+    };
+  }
+
   render() {
     return (
       <div>
         <NavBar />
+        <br />
+        <br />
+        <MainCategory categories={this.state.categories} />
       </div>
     );
   }
